672dc721c3d1439e32c44ea52438dc71e8f03abc
Neves-Rafael <nevesrafael.dev@gmail.com>
2023-10-25 23:27:41 -0300

    Init Commit

---
+++ src/components/Comment.jsx
@@ -0,0 +1,32 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/Neves-Rafael.png" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Rafael Neves</strong>
+              <time title="25 de Out as 14:00h" dateTime="2023-11-25 14:00:00">
+                1h atrás
+              </time>
+            </div>
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito bom, parabéns! 👏👏</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp size={24} />
+            Aplaudir
+            <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Post.jsx
@@ -0,0 +1,50 @@
+import style from "./Post.module.css";
+import { Comment } from "./comment";
+export function Post() {
+  return (
+    <article className={style.post}>
+      <header>
+        <div className={style.author}>
+          <img
+            className={style.avatar}
+            src="https://github.com/Neves-Rafael.png"
+          />
+          <div className={style.authorInfo}>
+            <strong>Rafael Neves</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time title="25 de Out as 14:00h" dateTime="2023-11-25 14:00:00">
+          Publicado há 1h
+        </time>
+      </header>
+      <div className={style.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat.
+        </p>
+        <p>
+          <a href="">👉 Jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto</a> <a>#rocketseat</a> <a>#nlw</a>
+        </p>
+      </div>
+      <form className={style.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comentário" />
+
+        <footer>
+          <button type="submit">Enviar</button>{" "}
+        </footer>
+      </form>
+
+      <div className={style.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
+    </article>
+  );
+}
